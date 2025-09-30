7d45bc8252ebdb2960724d6fbef3046922b8893d
p.brichev <p.brichev.sis@gmail.com>
2025-09-30 10:20:11 +0700

    baseUrl DI

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
-import { provideNativeDateAdapter } from '@angular/material/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { environment } from '../environments/environment';
+import { provideApi } from './api/index';
 
 import { routes } from './app.routes';
 
@@ -14,6 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideMomentDateAdapter(),
-    provideAnimations()
+    provideAnimations(),
+    provideApi(environment.apiBaseUrl)
   ],
 };
